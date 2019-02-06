fea08f78407d9ac1bafc16a345a136c30122463d
David Gleason <dgleason1990@gmail.com>
2019-02-06 14:11:51 -0500

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,8 @@ export default class Home extends Component {
     return (
       <div className='Home'>
         <h1> Welcome to Linq </h1>
+        <p> Linq is a website that allows people to connect with beauty and health providers around Toronto!</p>
+        <button></button>
       </div>
     )
   }
